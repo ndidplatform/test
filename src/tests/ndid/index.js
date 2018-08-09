189a130b2293148102409ac6b1d6c069c70be5c5
Raphassit Leelachaikul <raphassitlee@gmail.com>
2018-08-09 14:50:15 +0700

    Check for NDID node availability before running NDID API test

--- src/tests/ndid/index.js
+++ src/tests/ndid/index.js
@@ -1,4 +1,13 @@
+import { ndidAvailable } from '..';
+
 describe('NDID API', function() {
+  before(function() {
+    if (!ndidAvailable) {
+      this.test.parent.pending = true;
+      this.skip();
+    }
+  });
+
   require('./update_node');
   require('./error_response');
 });
