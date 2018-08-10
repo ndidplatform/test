66d49a65fc469031be4d361d4a0fbaa1c6f1fdcf
Jirapus <jirapus@blockfint.com>
2018-08-10 11:37:03 +0700

    Add more tests (RP close request the 2nd time)

--- src/tests/create_request/index.js
+++ src/tests/create_request/index.js
@@ -2,5 +2,6 @@ describe('(RP) Create request tests', function() {
   require('./timeout');
   require('./short_timeout');
   require('./long_timeout');
+  require('./close_request_2nd_time');
   require('./error_response');
 });
