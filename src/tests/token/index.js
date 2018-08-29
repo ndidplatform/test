164d76e26f1a4e2ddce8faeca98732eb033716b9
petch <tanee.kumpijit@gmail.com>
2018-08-29 17:35:53 +0700

    add wait to wait for token to settle

--- src/tests/token/index.js
+++ src/tests/token/index.js
@@ -1,11 +1,15 @@
 import { ndidAvailable } from '..';
+import { wait } from '../../utils';
 
-describe('Token tests', function() {
-  before(function() {
+describe('Token tests', async function() {
+  this.timeout(10000)
+  before(async function() {
     if (!ndidAvailable) {
       this.test.parent.pending = true;
       this.skip();
     }
+    //wait untill all token settle 
+    await wait(8000);
   });
 
   require('./node_token');
