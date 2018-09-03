85c903da27d5fbc3391bb098412094f59528c8ef
Jirapus <jirapus@blockfint.com>
2018-09-03 14:17:59 +0700

    Add more tests (idp error response)

--- src/tests/idp_response/error_response.js
+++ src/tests/idp_response/error_response.js
@@ -78,7 +78,7 @@ describe('IdP response errors tests', function() {
         incomingRequestPromise.resolve(callbackData);
       }
     });
-    
+
     const responseRp = await rpApi.createRequest('rp1', createRequestParams);
     const responseBodyRp = await responseRp.json();
     requestId = responseBodyRp.request_id;
@@ -308,6 +308,310 @@ describe('IdP response errors tests', function() {
     expect(responseBody.error.code).to.equal(20003);
   });
 
+  after(async function() {
+    this.timeout(10000);
+    await rpApi.closeRequest('rp1', {
+      reference_id: uuidv4(),
+      callback_url: config.RP_CALLBACK_URL,
+      request_id: requestId,
+    });
+    idp1EventEmitter.removeAllListeners('callback');
+    await wait(3000);
+  });
+});
+
+describe("IdP making response with ial less than request's min_ial and IdP making a response again test", function() {
+  let namespace;
+  let identifier;
+
+  const rpReferenceId = generateReferenceId();
+  const idpReferenceId = generateReferenceId(); //1st IDP
+
+  const incomingRequestPromise = createEventPromise(); // 1st IDP
+  const responseResultPromise = createEventPromise(); // 1st IDP
+
+  let createRequestParams;
+
+  let requestId;
+  let requestMessageHash;
+
+  before(async function() {
+    this.timeout(30000);
+    if (db.idp1Identities[0] == null) {
+      throw new Error('No created identity to use');
+    }
+
+    namespace = db.idp1Identities[0].namespace;
+    identifier = db.idp1Identities[0].identifier;
+
+    createRequestParams = {
+      reference_id: rpReferenceId,
+      callback_url: config.RP_CALLBACK_URL,
+      mode: 3,
+      namespace,
+      identifier,
+      idp_id_list: [],
+      data_request_list: [
+        {
+          service_id: 'bank_statement',
+          as_id_list: ['as1'],
+          min_as: 1,
+          request_params: JSON.stringify({
+            format: 'pdf',
+          }),
+        },
+        {
+          service_id: 'customer_info',
+          as_id_list: as2Available ? ['as2'] : ['as1'],
+          min_as: 1,
+          request_params: JSON.stringify({
+            format: 'pdf',
+          }),
+        },
+      ],
+      request_message:
+        'Test request message (error callback response) (mode 3)',
+      min_ial: 2.3,
+      min_aal: 3,
+      min_idp: 1,
+      request_timeout: 86400,
+    };
+
+    idp1EventEmitter.on('callback', function(callbackData) {
+      if (
+        callbackData.type === 'incoming_request' &&
+        callbackData.request_id === requestId
+      ) {
+        incomingRequestPromise.resolve(callbackData);
+      } else if (
+        callbackData.type === 'response_result' &&
+        callbackData.request_id === requestId
+      ) {
+        responseResultPromise.resolve(callbackData);
+      }
+    });
+
+    const responseRp = await rpApi.createRequest('rp1', createRequestParams);
+    const responseBodyRp = await responseRp.json();
+    requestId = responseBodyRp.request_id;
+    const incomingRequest = await incomingRequestPromise.promise;
+    requestMessageHash = incomingRequest.request_message_hash;
+  });
+
+  it("should get an error callback response when making a response with ial less than request's min_ial", async function() {
+    this.timeout(10000);
+    const identity = db.idp1Identities.find(
+      identity =>
+        identity.namespace === namespace && identity.identifier === identifier
+    );
+
+    const responseErrorCalback = await idpApi.createResponse('idp1', {
+      reference_id: idpReferenceId,
+      callback_url: config.IDP1_CALLBACK_URL,
+      request_id: requestId,
+      namespace: createRequestParams.namespace,
+      identifier: createRequestParams.identifier,
+      ial: 1.1,
+      aal: 3,
+      secret: identity.accessors[0].secret,
+      status: 'accept',
+      signature: createResponseSignature(
+        identity.accessors[0].accessorPrivateKey,
+        requestMessageHash
+      ),
+      accessor_id: identity.accessors[0].accessorId,
+    });
+    const responseBodyErrorCallback = await responseErrorCalback.json();
+
+    expect(responseErrorCalback.status).to.equal(400);
+    expect(responseBodyErrorCallback.error.code).to.equal(20055);
+  });
+
+  it('After IdP get an error callback response, IdP should making a response again successfully', async function() {
+    this.timeout(20000);
+    const identity = db.idp1Identities.find(
+      identity =>
+        identity.namespace === namespace && identity.identifier === identifier
+    );
+
+    const response = await idpApi.createResponse('idp1', {
+      reference_id: idpReferenceId,
+      callback_url: config.IDP1_CALLBACK_URL,
+      request_id: requestId,
+      namespace: createRequestParams.namespace,
+      identifier: createRequestParams.identifier,
+      ial: 2.3,
+      aal: 3,
+      secret: identity.accessors[0].secret,
+      status: 'accept',
+      signature: createResponseSignature(
+        identity.accessors[0].accessorPrivateKey,
+        requestMessageHash
+      ),
+      accessor_id: identity.accessors[0].accessorId,
+    });
+
+    expect(response.status).to.equal(202);
+
+    const responseResult = await responseResultPromise.promise;
+    expect(responseResult).to.deep.include({
+      reference_id: idpReferenceId,
+      request_id: requestId,
+      success: true,
+    });
+  });
+
+  after(async function() {
+    this.timeout(10000);
+    await rpApi.closeRequest('rp1', {
+      reference_id: uuidv4(),
+      callback_url: config.RP_CALLBACK_URL,
+      request_id: requestId,
+    });
+    idp1EventEmitter.removeAllListeners('callback');
+    await wait(3000);
+  });
+});
+
+describe("IdP making response with aal less than request's min_aal and IdP making a response again test", function() {
+  let namespace;
+  let identifier;
+
+  const rpReferenceId = generateReferenceId();
+  const idpReferenceId = generateReferenceId(); //1st IDP
+
+  const incomingRequestPromise = createEventPromise(); // 1st IDP
+  const responseResultPromise = createEventPromise(); // 1st IDP
+
+  let createRequestParams;
+
+  let requestId;
+  let requestMessageHash;
+
+  before(async function() {
+    this.timeout(30000);
+    if (db.idp1Identities[0] == null) {
+      throw new Error('No created identity to use');
+    }
+
+    namespace = db.idp1Identities[0].namespace;
+    identifier = db.idp1Identities[0].identifier;
+
+    createRequestParams = {
+      reference_id: rpReferenceId,
+      callback_url: config.RP_CALLBACK_URL,
+      mode: 3,
+      namespace,
+      identifier,
+      idp_id_list: [],
+      data_request_list: [
+        {
+          service_id: 'bank_statement',
+          as_id_list: ['as1'],
+          min_as: 1,
+          request_params: JSON.stringify({
+            format: 'pdf',
+          }),
+        },
+        {
+          service_id: 'customer_info',
+          as_id_list: as2Available ? ['as2'] : ['as1'],
+          min_as: 1,
+          request_params: JSON.stringify({
+            format: 'pdf',
+          }),
+        },
+      ],
+      request_message:
+        'Test request message (error callback response) (mode 3)',
+      min_ial: 2.3,
+      min_aal: 3,
+      min_idp: 1,
+      request_timeout: 86400,
+    };
+
+    idp1EventEmitter.on('callback', function(callbackData) {
+      if (
+        callbackData.type === 'incoming_request' &&
+        callbackData.request_id === requestId
+      ) {
+        incomingRequestPromise.resolve(callbackData);
+      } else if (
+        callbackData.type === 'response_result' &&
+        callbackData.request_id === requestId
+      ) {
+        responseResultPromise.resolve(callbackData);
+      }
+    });
+
+    const responseRp = await rpApi.createRequest('rp1', createRequestParams);
+    const responseBodyRp = await responseRp.json();
+    requestId = responseBodyRp.request_id;
+    const incomingRequest = await incomingRequestPromise.promise;
+    requestMessageHash = incomingRequest.request_message_hash;
+  });
+
+  it("should get an error callback response when making a response with aal less than request's min_aal", async function() {
+    this.timeout(10000);
+    const identity = db.idp1Identities.find(
+      identity =>
+        identity.namespace === namespace && identity.identifier === identifier
+    );
+
+    const responseErrorCalback = await idpApi.createResponse('idp1', {
+      reference_id: idpReferenceId,
+      callback_url: config.IDP1_CALLBACK_URL,
+      request_id: requestId,
+      namespace: createRequestParams.namespace,
+      identifier: createRequestParams.identifier,
+      ial: 2.3,
+      aal: 1,
+      secret: identity.accessors[0].secret,
+      status: 'accept',
+      signature: createResponseSignature(
+        identity.accessors[0].accessorPrivateKey,
+        requestMessageHash
+      ),
+      accessor_id: identity.accessors[0].accessorId,
+    });
+    const responseBodyErrorCallback = await responseErrorCalback.json();
+
+    expect(responseErrorCalback.status).to.equal(400);
+    expect(responseBodyErrorCallback.error.code).to.equal(20056);
+  });
+
+  it('After IdP get an error callback response, IdP should making a response again successfully', async function() {
+    this.timeout(20000);
+    const identity = db.idp1Identities.find(
+      identity =>
+        identity.namespace === namespace && identity.identifier === identifier
+    );
+    const response = await idpApi.createResponse('idp1', {
+      reference_id: idpReferenceId,
+      callback_url: config.IDP1_CALLBACK_URL,
+      request_id: requestId,
+      namespace: createRequestParams.namespace,
+      identifier: createRequestParams.identifier,
+      ial: 2.3,
+      aal: 3,
+      secret: identity.accessors[0].secret,
+      status: 'accept',
+      signature: createResponseSignature(
+        identity.accessors[0].accessorPrivateKey,
+        requestMessageHash
+      ),
+      accessor_id: identity.accessors[0].accessorId,
+    });
+
+    expect(response.status).to.equal(202);
+
+    const responseResult = await responseResultPromise.promise;
+    expect(responseResult).to.deep.include({
+      reference_id: idpReferenceId,
+      request_id: requestId,
+      success: true,
+    });
+  });
   after(async function() {
     this.timeout(10000);
     await rpApi.closeRequest('rp1', {
