ebbe42eb4cb2fc5b59dcfa63b317454fba657cae
Jirapus <jirapus@blockfint.com>
2018-09-18 12:03:38 +0700

    Rename file test

--- src/tests/idp_response/index.js
+++ src/tests/idp_response/index.js
@@ -1,5 +1,5 @@
 describe('(IdP) Response tests', function() {
   require('./error_response');
-  require('./response_request_final_state');
+  require('./response_request_final_stage');
   require('./response_invalid_data');
 });
