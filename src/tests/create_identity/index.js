640eff32ebeeb745945deadac193f2fa122bcbfa
Raphassit Leelachaikul <raphassitlee@gmail.com>
2018-09-24 17:49:02 +0700

    Cache clear debug

--- src/tests/create_identity/index.js
+++ src/tests/create_identity/index.js
@@ -3,11 +3,11 @@ import { wait } from '../../utils';
 
 describe('Create identity', function() {
   require('./1st_idp');
-  require('./2nd_idp');
-  require('./lock_timeout');
-  require('./update_identity_ial');
-  require('./close_identity_request');
-  require('./error_response');
+  // require('./2nd_idp');
+  // require('./lock_timeout');
+  // require('./update_identity_ial');
+  // require('./close_identity_request');
+  // require('./error_response');
 
   after(async function() {
     //wait for identity to propagate (different abci/tendermint for idp/rp)
