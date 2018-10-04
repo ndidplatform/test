f76f5461d354e19c29539d7a0f51b64baf580afe
Jirapus <jirapus@blockfint.com>
2018-10-04 11:42:14 +0700

    Change resolve create_request_result callback from check reference_id to check request_id

--- src/tests/create_request/duplicate_reference_id.js
+++ src/tests/create_request/duplicate_reference_id.js
@@ -93,11 +93,13 @@ describe('Create request with duplicate reference id that is not in progress (cl
   const rpReferenceId = generateReferenceId();
   const rpCloseRequestReferenceId = generateReferenceId();
 
-  const createRequestResultPromise = createEventPromise(); //RP
+  const createRequestResultPromise1st = createEventPromise(); //RP
+  const createRequestResultPromise2nd = createEventPromise(); //RP
   const closeRequestResultPromise = createEventPromise(); //RP
 
   let createRequestParams;
-  let requestId;
+  let requestId1st;
+  let requestId2nd;
 
   before(async function() {
     this.timeout(10000);
@@ -127,9 +129,14 @@ describe('Create request with duplicate reference id that is not in progress (cl
     rpEventEmitter.on('callback', function(callbackData) {
       if (
         callbackData.type === 'create_request_result' &&
-        callbackData.reference_id === rpReferenceId
+        callbackData.request_id === requestId1st
       ) {
-        createRequestResultPromise.resolve(callbackData);
+        createRequestResultPromise1st.resolve(callbackData);
+      } else if (
+        callbackData.type === 'create_request_result' &&
+        callbackData.request_id === requestId2nd
+      ) {
+        createRequestResultPromise2nd.resolve(callbackData);
       } else if (
         callbackData.type === 'close_request_result' &&
         callbackData.reference_id === rpCloseRequestReferenceId
@@ -140,8 +147,8 @@ describe('Create request with duplicate reference id that is not in progress (cl
 
     let response = await rpApi.createRequest('rp1', createRequestParams);
     let responseBody = await response.json();
-    await createRequestResultPromise.promise;
-    requestId = responseBody.request_id;
+    requestId1st = responseBody.request_id;
+    await createRequestResultPromise1st.promise;
     await wait(2000);
   });
 
@@ -158,17 +165,17 @@ describe('Create request with duplicate reference id that is not in progress (cl
     const response = await rpApi.closeRequest('rp1', {
       reference_id: rpCloseRequestReferenceId,
       callback_url: config.RP_CALLBACK_URL,
-      request_id: requestId,
+      request_id: requestId1st,
     });
     expect(response.status).to.equal(202);
 
     const closeRequestResult = await closeRequestResultPromise.promise;
     expect(closeRequestResult).to.deep.include({
       reference_id: rpCloseRequestReferenceId,
-      request_id: requestId,
+      request_id: requestId1st,
       success: true,
     });
-    await wait(2000);
+    await wait(3000);
   });
 
   it('After request duplicate reference id is not in progress (closed) RP should create a request successfully', async function() {
@@ -179,9 +186,9 @@ describe('Create request with duplicate reference id that is not in progress (cl
     expect(responseBody.request_id).to.be.a('string').that.is.not.empty;
     expect(responseBody.initial_salt).to.be.a('string').that.is.not.empty;
 
-    requestId = responseBody.request_id;
+    requestId2nd = responseBody.request_id;
 
-    const createRequestResult = await createRequestResultPromise.promise;
+    const createRequestResult = await createRequestResultPromise2nd.promise;
     expect(createRequestResult.success).to.equal(true);
   });
 
@@ -190,7 +197,7 @@ describe('Create request with duplicate reference id that is not in progress (cl
     await rpApi.closeRequest('rp1', {
       reference_id: rpCloseRequestReferenceId,
       callback_url: config.RP_CALLBACK_URL,
-      request_id: requestId,
+      request_id: requestId2nd,
     });
     rpEventEmitter.removeAllListeners('callback');
     await wait(3000);
@@ -204,11 +211,13 @@ describe('Create request with duplicate reference id that is not in progress (ti
   const rpReferenceId = generateReferenceId();
   const rpCloseRequestReferenceId = generateReferenceId();
 
-  const createRequestResultPromise = createEventPromise(); //RP
+  const createRequestResultPromise1st = createEventPromise(); //RP
+  const createRequestResultPromise2nd = createEventPromise(); //RP
   const requestStatusTimedOutPromise = createEventPromise(); // RP
 
   let createRequestParams;
-  let requestId;
+  let requestId1st;
+  let requestId2nd;
 
   before(async function() {
     this.timeout(10000);
@@ -232,18 +241,23 @@ describe('Create request with duplicate reference id that is not in progress (ti
       min_ial: 1.1,
       min_aal: 1,
       min_idp: 1,
-      request_timeout: 10, //sec
+      request_timeout: 7, //sec
     };
 
     rpEventEmitter.on('callback', function(callbackData) {
       if (
         callbackData.type === 'create_request_result' &&
-        callbackData.reference_id === rpReferenceId
+        callbackData.request_id === requestId1st
       ) {
-        createRequestResultPromise.resolve(callbackData);
+        createRequestResultPromise1st.resolve(callbackData);
+      } else if (
+        callbackData.type === 'create_request_result' &&
+        callbackData.request_id === requestId2nd
+      ) {
+        createRequestResultPromise2nd.resolve(callbackData);
       } else if (
         callbackData.type === 'request_status' &&
-        callbackData.request_id === requestId
+        callbackData.request_id === requestId1st
       ) {
         if (callbackData.status === 'pending') {
           if (callbackData.timed_out) {
@@ -255,8 +269,8 @@ describe('Create request with duplicate reference id that is not in progress (ti
 
     let response = await rpApi.createRequest('rp1', createRequestParams);
     let responseBody = await response.json();
-    await createRequestResultPromise.promise;
-    requestId = responseBody.request_id;
+    requestId1st = responseBody.request_id;
+    await createRequestResultPromise1st.promise;
     await wait(2000);
   });
 
@@ -272,7 +286,7 @@ describe('Create request with duplicate reference id that is not in progress (ti
     this.timeout(20000);
     const requestStatus = await requestStatusTimedOutPromise.promise;
     expect(requestStatus).to.deep.include({
-      request_id: requestId,
+      request_id: requestId1st,
       status: 'pending',
       mode: createRequestParams.mode,
       min_idp: createRequestParams.min_idp,
@@ -294,9 +308,9 @@ describe('Create request with duplicate reference id that is not in progress (ti
     expect(responseBody.request_id).to.be.a('string').that.is.not.empty;
     expect(responseBody.initial_salt).to.be.a('string').that.is.not.empty;
 
-    requestId = responseBody.request_id;
+    requestId2nd = responseBody.request_id;
 
-    const createRequestResult = await createRequestResultPromise.promise;
+    const createRequestResult = await createRequestResultPromise2nd.promise;
     expect(createRequestResult.success).to.equal(true);
   });
 
@@ -305,7 +319,7 @@ describe('Create request with duplicate reference id that is not in progress (ti
     await rpApi.closeRequest('rp1', {
       reference_id: rpCloseRequestReferenceId,
       callback_url: config.RP_CALLBACK_URL,
-      request_id: requestId,
+      request_id: requestId2nd,
     });
     rpEventEmitter.removeAllListeners('callback');
     await wait(3000);
