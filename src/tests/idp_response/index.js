514aef1bbf9066b134ca8ed1feb7797a4e3487ff
Jirapus <jirapus@blockfint.com>
2018-10-05 14:46:58 +0700

    Add more tests (IdP error callback response tests)

--- src/tests/idp_response/index.js
+++ src/tests/idp_response/index.js
@@ -2,4 +2,5 @@ describe('(IdP) Response tests', function() {
   require('./error_response');
   require('./response_request_final_stage');
   require('./response_invalid_data');
+  require('./error_callback_response');
 });
