490d9457b2622b531d167e57a87ff42a0cce867f
Jirapus <jirapus@blockfint.com>
2018-10-11 16:19:42 +0700

    Comment test step that result will non-deterministic

--- src/tests/data_request/1_idp_2_as_2_service_mode_3.js
+++ src/tests/data_request/1_idp_2_as_2_service_mode_3.js
@@ -528,43 +528,43 @@ describe('1 IdP, 2 AS (min_as = 2), 2 Services, mode 3', function() {
     });
   });
 
-  it('RP should receive request status with both services (bank_statement and customer_info) signed data count = 2', async function() {
-    this.timeout(25000);
-    const requestStatus = await requestStatusSignedDataPromise.promise;
-    expect(requestStatus).to.deep.include({
-      request_id: requestId,
-      status: 'confirmed',
-      mode: createRequestParams.mode,
-      min_idp: createRequestParams.min_idp,
-      answered_idp_count: 1,
-      closed: false,
-      timed_out: false,
-      service_list: [
-        {
-          service_id: createRequestParams.data_request_list[0].service_id,
-          min_as: createRequestParams.data_request_list[0].min_as,
-          signed_data_count: 2,
-          received_data_count: 0,
-        },
-        {
-          service_id: createRequestParams.data_request_list[1].service_id,
-          min_as: createRequestParams.data_request_list[1].min_as,
-          signed_data_count: 2,
-          received_data_count: 0,
-        },
-      ],
-      response_valid_list: [
-        {
-          idp_id: 'idp1',
-          valid_signature: true,
-          valid_proof: true,
-          valid_ial: true,
-        },
-      ],
-    });
-    expect(requestStatus).to.have.property('block_height');
-    expect(requestStatus.block_height).is.a('number');
-  });
+  // it('RP should receive request status with both services (bank_statement and customer_info) signed data count = 2', async function() {
+  //   this.timeout(25000);
+  //   const requestStatus = await requestStatusSignedDataPromise.promise;
+  //   expect(requestStatus).to.deep.include({
+  //     request_id: requestId,
+  //     status: 'confirmed',
+  //     mode: createRequestParams.mode,
+  //     min_idp: createRequestParams.min_idp,
+  //     answered_idp_count: 1,
+  //     closed: false,
+  //     timed_out: false,
+  //     service_list: [
+  //       {
+  //         service_id: createRequestParams.data_request_list[0].service_id,
+  //         min_as: createRequestParams.data_request_list[0].min_as,
+  //         signed_data_count: 2,
+  //         received_data_count: 0,
+  //       },
+  //       {
+  //         service_id: createRequestParams.data_request_list[1].service_id,
+  //         min_as: createRequestParams.data_request_list[1].min_as,
+  //         signed_data_count: 2,
+  //         received_data_count: 0,
+  //       },
+  //     ],
+  //     response_valid_list: [
+  //       {
+  //         idp_id: 'idp1',
+  //         valid_signature: true,
+  //         valid_proof: true,
+  //         valid_ial: true,
+  //       },
+  //     ],
+  //   });
+  //   expect(requestStatus).to.have.property('block_height');
+  //   expect(requestStatus.block_height).is.a('number');
+  // });
 
   it('RP should receive completed request status with both services (bank_statement and customer_info) received data count = 2', async function() {
     this.timeout(15000);
