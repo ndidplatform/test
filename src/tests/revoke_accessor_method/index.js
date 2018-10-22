71329b992bf6678e074fa559e911ac3fc54341a1
Jirapus <jirapus@blockfint.com>
2018-10-22 13:02:09 +0700

    Add more test (Reject revoke accessor test)

--- src/tests/revoke_accessor_method/index.js
+++ src/tests/revoke_accessor_method/index.js
@@ -1,6 +1,7 @@
 describe('Revoke accessor', function() {
   require('./revoke_fail');
   require('./revoke_success');
+  require('./reject_revoke_accessor');
   require('./duplicate_reference_id');
   require('./close_revoke_accessor_request');
   require('./error_response');
