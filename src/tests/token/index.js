0a62afffe9401b2859366cab0fcef3e5ea351943
Jirapus <jirapus@blockfint.com>
2019-03-13 12:35:54 +0700

    Add more test (Reduce node token when tx fail test)

--- src/tests/token/index.js
+++ src/tests/token/index.js
@@ -2,17 +2,17 @@ import { ndidAvailable } from '..';
 import { wait } from '../../utils';
 
 describe('Token tests', async function() {
-  this.timeout(10000)
+  this.timeout(10000);
   before(async function() {
     if (!ndidAvailable) {
       this.test.parent.pending = true;
       this.skip();
     }
-    //wait untill all token settle 
+    //wait untill all token settle
     await wait(8000);
   });
 
   require('./node_token');
   require('./spend_and_refill_node_token');
-
+  require('./reduce_token_fail_tx');
 });
