cd7574021f534b8385b3563e647ed5ac7b538ec7
Jirapus <jirapus@blockfint.com>
2019-04-10 10:35:33 +0700

    Add more tests (create identity)

--- src/tests/v3/create_identity/index.js
+++ src/tests/v3/create_identity/index.js
@@ -2,3 +2,8 @@ require('./1st_idp_mode_2');
 require('./2nd_idp_mode_2');
 require('./1st_idp_mode_3');
 require('./2nd_idp_mode_3');
+require('./1st_idp_mode_2_2nd_idp_mode_3');
+require('./1st_idp_mode_3_2nd_idp_mode_2');
+require('./create_multiple_identifier_mode_2');
+require('./create_multiple_identifier_mode_3');
+
