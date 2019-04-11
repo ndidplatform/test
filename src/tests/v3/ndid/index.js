cc9c1552156c5713bf2c3adb7bd9cb3c1068bd4e
Jirapus <jirapus@blockfint.com>
2019-04-11 16:49:55 +0700

    Add test skipping

--- src/tests/v3/ndid/index.js
+++ src/tests/v3/ndid/index.js
@@ -1,4 +1,11 @@
+import { ndidAvailable } from '../..';
 describe('NDID tests', function() {
+  before(function() {
+    if (!ndidAvailable) {
+      this.test.parent.pending = true;
+      this.skip();
+    }
+  });
   require('./add_namespace');
   require('./set_allowed_min_ial_for_register_identity_at_first_idp');
   require('./set_allowed_mode_list_for_create_request');
