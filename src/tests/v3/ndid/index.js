00d5cb8ee903294e974a7ce8c878e20aa6954180
Jirapus <jirapus@blockfint.com>
2019-04-11 16:47:54 +0700

    Add more tests (NDID set allowed min ial for register identity at first idp test)

--- src/tests/v3/ndid/index.js
+++ src/tests/v3/ndid/index.js
@@ -1,5 +1,5 @@
 describe('NDID tests', function() {
   require('./add_namespace');
-  // require('./set_allowed_min_ial_for_register_identity_at_first_idp');
+  require('./set_allowed_min_ial_for_register_identity_at_first_idp');
   require('./set_allowed_mode_list_for_create_request');
 });
