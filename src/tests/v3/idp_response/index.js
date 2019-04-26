3841f72ee6ea70a4aa4230a1859c7dc13062a8c6
Jirapus <jirapus@blockfint.com>
2019-04-26 19:35:32 +0700

    Add test case and remove unused test case

--- src/tests/v3/idp_response/index.js
+++ src/tests/v3/idp_response/index.js
@@ -1,6 +1,7 @@
 describe('(IdP) Response tests', function() {
   require('./error_response');
   require('./response_request_final_stage');
-  //require('./response_invalid_data');
+  
+  //require('./response_invalid_data');  MOVE TO ./error_response
   //require('./error_callback_response'); MOVED TO ./error_response
 });
--- src/tests/v3/revoke_accessor/index.js
+++ src/tests/v3/revoke_accessor/index.js
@@ -1,5 +1,8 @@
 describe('Revoke accessor tests', function() {
   require('./revoke_accessor_mode_2');
   require('./revoke_accessor_mode_3');
+  require('./revoke_accessor_mode_3_only_1_idp');
+  require('./revoke_last_accessor_mode_3');
+  require('./duplicate_reference_id');
   require('./error_response');
 });
