af8681f84578da08673f76bd0319d39b9b6f42ae
Jirapus <jirapus@blockfint.com>
2019-04-29 10:50:23 +0700

    Add more test (revoke identity error response test)

--- src/tests/v3/revoke_identity_association/index.js
+++ src/tests/v3/revoke_identity_association/index.js
@@ -2,4 +2,5 @@ describe('Revoke identity association', function() {
   require('./revoke_identity_association_mode_2');
   require('./revoke_identity_association_mode_3');
   require('./revoke_identity_association_mode_3_only_1_idp');
+  require('./error_response');
 });
