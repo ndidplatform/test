7b0e605ad054469a52f25ff8bafba127b7c87a15
Jirapus <jirapus@blockfint.com>
2019-05-08 10:47:30 +0700

    Add more test create identity errors

--- src/tests/v3/create_identity/error_response.js
+++ src/tests/v3/create_identity/error_response.js
@@ -3,8 +3,10 @@ import forge from 'node-forge';
 import uuidv4 from 'uuid/v4';
 
 import * as identityApi from '../../../api/v3/identity';
+import * as commonApi from '../../../api/v3/common';
 import * as db from '../../../db';
-import { generateReferenceId } from '../../../utils';
+import { idp1EventEmitter } from '../../../callback_server';
+import { generateReferenceId, wait, createEventPromise } from '../../../utils';
 import * as config from '../../../config';
 
 describe('Create identity errors', function() {
@@ -186,5 +188,318 @@ MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEZYQxuM06/obj3ae0R2UUTt/JWrnvDzx+
     const responseBody = await response.json();
     expect(response.status).to.equal(400);
     expect(responseBody.error.code).to.equal(20044);
+  });
+});
+
+describe('Create identity errors', function() {
+  let namespace = 'citizen_id';
+  let identifier = uuidv4();
+  let identifier2 = uuidv4();
+  const keypair = forge.pki.rsa.generateKeyPair(2048);
+  const accessorPublicKey = forge.pki.publicKeyToPem(keypair.publicKey);
+
+  const referenceId = generateReferenceId();
+  const referenceId2 = generateReferenceId();
+
+  const createIdentityResultPromise = createEventPromise();
+  const createIdentityResultPromise2 = createEventPromise();
+
+  let accessorId;
+  let referenceGroupCode;
+
+  before(function() {
+    idp1EventEmitter.on('callback', function(callbackData) {
+      if (
+        callbackData.type === 'create_identity_result' &&
+        callbackData.reference_id === referenceId
+      ) {
+        createIdentityResultPromise.resolve(callbackData);
+      } else if (
+        callbackData.type === 'create_identity_result' &&
+        callbackData.reference_id === referenceId2
+      ) {
+        createIdentityResultPromise2.resolve(callbackData);
+      }
+    });
+  });
+
+  it('Before create identity this sid should not exist on platform ', async function() {
+    const response = await identityApi.getIdentityInfo('idp1', {
+      namespace,
+      identifier,
+    });
+    expect(response.status).to.equal(404);
+  });
+
+  it('Before create identity this sid should not associated with idp1 ', async function() {
+    const response = await commonApi.getRelevantIdpNodesBySid('idp1', {
+      namespace,
+      identifier,
+    });
+    const idpNodes = await response.json();
+    const idpNode = idpNodes.find(idpNode => idpNode.node_id === 'idp1');
+    expect(idpNode).to.be.an.undefined;
+  });
+
+  it('Before create identity should not get identity ial', async function() {
+    const response = await identityApi.getIdentityIal('idp1', {
+      namespace,
+      identifier,
+    });
+    expect(response.status).to.equal(404);
+  });
+
+  it('Should create identity request (mode 2) successfully', async function() {
+    this.timeout(10000);
+    const response = await identityApi.createIdentity('idp1', {
+      reference_id: referenceId,
+      callback_url: config.IDP1_CALLBACK_URL,
+      identity_list: [
+        {
+          namespace,
+          identifier,
+        },
+      ],
+      accessor_type: 'RSA',
+      accessor_public_key: accessorPublicKey,
+      //accessor_id,
+      ial: 2.3,
+      mode: 2,
+    });
+    const responseBody = await response.json();
+    expect(response.status).to.equal(202);
+    expect(responseBody.accessor_id).to.be.a('string').that.is.not.empty;
+    expect(responseBody.exist).to.equal(false);
+
+    accessorId = responseBody.accessor_id;
+  });
+
+  it('Identity should be created successfully', async function() {
+    this.timeout(15000);
+    const createIdentityResult = await createIdentityResultPromise.promise;
+    expect(createIdentityResult).to.deep.include({
+      reference_id: referenceId,
+      success: true,
+    });
+
+    expect(createIdentityResult.reference_group_code).to.be.a('string').that.is
+      .not.empty;
+
+    referenceGroupCode = createIdentityResult.reference_group_code;
+
+    const response = await commonApi.getRelevantIdpNodesBySid('idp1', {
+      namespace,
+      identifier,
+    });
+    const idpNodes = await response.json();
+    const idpNode = idpNodes.find(idpNode => idpNode.node_id === 'idp1');
+    expect(idpNode).to.not.be.undefined;
+    expect(idpNode.mode_list)
+      .to.be.an('array')
+      .that.include(2);
+    await wait(1500);
+  });
+
+  it('After create identity this sid should be existing on platform ', async function() {
+    const response = await identityApi.getIdentityInfo('idp1', {
+      namespace,
+      identifier,
+    });
+    expect(response.status).to.equal(200);
+    const responseBody = await response.json();
+    expect(responseBody.reference_group_code).to.equal(referenceGroupCode);
+  });
+
+  it('After create identity should get identity ial successfully', async function() {
+    const response = await identityApi.getIdentityIal('idp1', {
+      namespace,
+      identifier,
+    });
+    expect(response.status).to.equal(200);
+    const responseBody = await response.json();
+    expect(responseBody.ial).to.equal(2.3);
+  });
+
+  it('Before create identity this sid should not exist on platform ', async function() {
+    const response = await identityApi.getIdentityInfo('idp1', {
+      namespace,
+      identifier: identifier2,
+    });
+    expect(response.status).to.equal(404);
+  });
+
+  it('Before create identity this sid should not associated with idp1 ', async function() {
+    const response = await commonApi.getRelevantIdpNodesBySid('idp1', {
+      namespace,
+      identifier: identifier2,
+    });
+    const idpNodes = await response.json();
+    const idpNode = idpNodes.find(idpNode => idpNode.node_id === 'idp1');
+    expect(idpNode).to.be.an.undefined;
+  });
+
+  it('Before create identity should not get identity ial', async function() {
+    const response = await identityApi.getIdentityIal('idp1', {
+      namespace,
+      identifier: identifier2,
+    });
+    expect(response.status).to.equal(404);
+  });
+
+  it('Should create identity request (mode 2) successfully', async function() {
+    this.timeout(10000);
+    const response = await identityApi.createIdentity('idp1', {
+      reference_id: referenceId2,
+      callback_url: config.IDP1_CALLBACK_URL,
+      identity_list: [
+        {
+          namespace,
+          identifier: identifier2,
+        },
+      ],
+      accessor_type: 'RSA',
+      accessor_public_key: accessorPublicKey,
+      //accessor_id,
+      ial: 2.3,
+      mode: 2,
+    });
+    const responseBody = await response.json();
+    expect(response.status).to.equal(202);
+    expect(responseBody.accessor_id).to.be.a('string').that.is.not.empty;
+    expect(responseBody.exist).to.equal(false);
+
+    accessorId = responseBody.accessor_id;
+  });
+
+  it('Identity should be created successfully', async function() {
+    this.timeout(15000);
+    const createIdentityResult = await createIdentityResultPromise2.promise;
+    expect(createIdentityResult).to.deep.include({
+      reference_id: referenceId2,
+      success: true,
+    });
+
+    expect(createIdentityResult.reference_group_code).to.be.a('string').that.is
+      .not.empty;
+
+    referenceGroupCode = createIdentityResult.reference_group_code;
+
+    const response = await commonApi.getRelevantIdpNodesBySid('idp1', {
+      namespace,
+      identifier: identifier2,
+    });
+    const idpNodes = await response.json();
+    const idpNode = idpNodes.find(idpNode => idpNode.node_id === 'idp1');
+    expect(idpNode).to.not.be.undefined;
+    expect(idpNode.mode_list)
+      .to.be.an('array')
+      .that.include(2);
+    await wait(1500);
+  });
+
+  it('After create identity this sid should be existing on platform ', async function() {
+    const response = await identityApi.getIdentityInfo('idp1', {
+      namespace,
+      identifier: identifier2,
+    });
+    expect(response.status).to.equal(200);
+    const responseBody = await response.json();
+    expect(responseBody.reference_group_code).to.equal(referenceGroupCode);
+  });
+
+  it('After create identity should get identity ial successfully', async function() {
+    const response = await identityApi.getIdentityIal('idp1', {
+      namespace,
+      identifier: identifier2,
+    });
+    expect(response.status).to.equal(200);
+    const responseBody = await response.json();
+    expect(responseBody.ial).to.equal(2.3);
+  });
+
+  it('IdP should get an error when creating duplicate identity in identity_list', async function() {
+    this.timeout(10000);
+    const response = await identityApi.createIdentity('idp1', {
+      reference_id: referenceId,
+      callback_url: config.IDP1_CALLBACK_URL,
+      identity_list: [
+        {
+          namespace,
+          identifier,
+        },
+        {
+          namespace,
+          identifier,
+        },
+      ],
+      accessor_type: 'RSA',
+      accessor_public_key: accessorPublicKey,
+      //accessor_id,
+      ial: 2.3,
+      mode: 3,
+    });
+    expect(response.status).to.equal(400);
+    const responseBody = await response.json();
+    expect(responseBody.error.code).to.equal(20003);
+  });
+
+  it('IdP should get an error when input sid already onboard in this idp in identity_list', async function() {
+    this.timeout(10000);
+
+    const response = await identityApi.createIdentity('idp1', {
+      reference_id: referenceId,
+      callback_url: config.IDP1_CALLBACK_URL,
+      identity_list: [
+        {
+          namespace,
+          identifier,
+        },
+        {
+          namespace,
+          identifier: uuidv4(),
+        },
+      ],
+      accessor_type: 'RSA',
+      accessor_public_key: accessorPublicKey,
+      //accessor_id,
+      ial: 2.3,
+      mode: 2,
+    });
+    expect(response.status).to.equal(400);
+    const responseBody = await response.json();
+    expect(responseBody.error.code).to.equal(20019);
+  });
+
+  it('IdP (idp2) should get an error when creating identity with multiple reference group in identity list', async function() {
+    this.timeout(100000);
+    const response = await identityApi.createIdentity('idp2', {
+      reference_id: referenceId,
+      callback_url: config.IDP1_CALLBACK_URL,
+      identity_list: [
+        {
+          namespace,
+          identifier,
+        },
+        {
+          namespace,
+          identifier: identifier2,
+        },
+        {
+          namespace,
+          identifier: uuidv4(),
+        },
+      ],
+      accessor_type: 'RSA',
+      accessor_public_key: accessorPublicKey,
+      //accessor_id,
+      ial: 2.3,
+      mode: 2,
+    });
+    expect(response.status).to.equal(400);
+    const responseBody = await response.json();
+    expect(responseBody.error.code).to.equal(20069);
+  });
+  after(function() {
+    idp1EventEmitter.removeAllListeners('callback');
   });
 });
