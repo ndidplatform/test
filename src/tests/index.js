0eeb768a21507dbb0470b55ba727f78609ff2293
Jirapus <jirapus@blockfint.com>
2019-06-26 15:31:57 +0700

    Remove unused code

--- src/tests/index.js
+++ src/tests/index.js
@@ -65,24 +65,6 @@ describe('End-to-End NDID API test (API v3)', function() {
   });
 
   require('./v3');
-  // require('./dpki_setup');
-  // require('./rp_setup');
-  // require('./idp_setup');
-  // require('./as_setup');
-  // require('./as_service_setup');
-  // require('./proxy_setup');
-  // require('./create_identity');
-  // require('./ndid');
-  // require('./dpki');
-  // require('./add_accessor_method');
-  // require('./revoke_accessor_method');
-  // require('./verify_identity');
-  // require('./data_request');
-  // require('./create_request');
-  // require('./idp_response');
-  // require('./as_data_response');
-  // require('./token');
-  // require('./ndid_disable_enable');
 
   after(function() {
     stopCallbackServers();
