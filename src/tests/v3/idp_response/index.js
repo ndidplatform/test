5952653fcabcfec2e607ce9bbd56c2de76cbdadf
Jirapus <jirapus@blockfint.com>
2019-07-09 22:33:57 +0700

    Add more test (idp response invalid accessor id)

--- src/tests/v3/idp_response/index.js
+++ src/tests/v3/idp_response/index.js
@@ -2,6 +2,7 @@ describe('(IdP) Response tests', function() {
   require('./error_response');
   require('./response_request_final_stage');
   require('./error_get_request_message_padded_hash');
+  require('./response_invalid_accessor_id');
 
   //require('./response_invalid_data');  MOVE TO ./error_response
   //require('./error_callback_response'); MOVED TO ./error_response
