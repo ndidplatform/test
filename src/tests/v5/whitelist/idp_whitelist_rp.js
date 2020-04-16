311d0cf0e78ee5b214cbfd3339fd366aa175ec63
Jirapus <jirapus@blockfint.com>
2020-04-16 15:01:20 +0700

    Fix idp whitelist rp for test working

--- src/tests/v5/whitelist/idp_whitelist_rp.js
+++ src/tests/v5/whitelist/idp_whitelist_rp.js
@@ -5,7 +5,6 @@ import uuidv4 from 'uuid/v4';
 import * as identityApi from '../../../api/v5/identity';
 import * as commonApi from '../../../api/v5/common';
 import * as rpApi from '../../../api/v5/rp';
-import * as idpApi from '../../../api/v5/idp';
 import {
   idp3EventEmitter,
   rp2EventEmitter,
@@ -22,17 +21,11 @@ import {
   wait,
   createSignature,
   createResponseSignature,
-  hash,
 } from '../../../utils';
 import { ndidAvailable, idp3Available, rp2Available } from '../..';
 import { mode1DataRequestFlowTest } from '../_fragments/data_request_mode_1_flow';
 import { mode2And3DataRequestFlowTest } from '../_fragments/data_request_mode_2_and_3_flow';
-import { eventEmitter as nodeCallbackEventEmitter } from '../../../callback_server/node';
-import { receiveMessagequeueSendSuccessCallback } from '../_fragments/common';
-import {
-  verifyResponseSignature,
-  getAndVerifyRequestMessagePaddedHashTest,
-} from '../_fragments/request_flow_fragments/idp';
+
 
 describe('IdP whitelist RP tests', function () {
   before(function () {
@@ -655,8 +648,6 @@ describe('IdP whitelist RP tests', function () {
   });
 
   describe('IdP (idp2) create identity (mode 3) (without providing accessor_id) as 2nd IdP (already onboard at idp3)', function () {
-    //TODO
-
     let namespace;
     let identifier;
 
@@ -683,23 +674,13 @@ describe('IdP whitelist RP tests', function () {
     const createIdentityRequestResultPromise = createEventPromise(); // idp2
     const createIdentityResultPromise = createEventPromise(); // idp2
 
-    const mqSendSuccessIdp2ToIdp3CallbackPromise = createEventPromise();
-    const mqSendSuccessIdp3ToIdp2CallbackPromise = createEventPromise();
-
-    //const IdP2notificationCreateIdentityPromise = createEventPromise();
-
     let requestId;
     let accessorId;
-    let requestMessagePaddedHash;
     let referenceGroupCode;
-    //   let requestMessageHash;
     const createIdentityRequestMessage =
       'Create identity consent request custom message ข้อความสำหรับขอสร้างตัวตนบนระบบ';
-    let identityForResponse;
-    let responseAccessorId;
 
     before(function () {
-      this.skip();
       if (!idp3Available) {
         this.test.parent.pending = true;
         this.skip();
@@ -758,35 +739,6 @@ describe('IdP whitelist RP tests', function () {
           callbackData.reference_id === referenceId
         ) {
           createIdentityResultPromise.resolve(callbackData);
-        }
-      });
-
-      // idp2EventEmitter.on('identity_notification_callback', function(
-      //   callbackData
-      // ) {
-      //   if (
-      //     callbackData.type === 'identity_modification_notification' &&
-      //     callbackData.reference_group_code === referenceGroupCode &&
-      //     callbackData.action === 'create_identity'
-      //   ) {
-      //     IdP2notificationCreateIdentityPromise.resolve(callbackData);
-      //   }
-      // });
-
-      nodeCallbackEventEmitter.on('callback', function (callbackData) {
-        if (
-          callbackData.type === 'message_queue_send_success' &&
-          callbackData.request_id === requestId
-        ) {
-          if (callbackData.node_id === 'idp2') {
-            if (callbackData.destination_node_id === 'idp3') {
-              mqSendSuccessIdp2ToIdp3CallbackPromise.resolve(callbackData);
-            }
-          } else if (callbackData.node_id === 'idp3') {
-            if (callbackData.destination_node_id === 'idp2') {
-              mqSendSuccessIdp3ToIdp2CallbackPromise.resolve(callbackData);
-            }
-          }
         }
       });
     });
@@ -852,261 +804,10 @@ describe('IdP whitelist RP tests', function () {
       expect(createIdentityRequestResult).to.deep.include({
         reference_id: referenceId,
         request_id: requestId,
-        exist: true,
         accessor_id: accessorId,
-        success: true,
+        success: false,
+        error: { code: 25074, message: 'Node is not in whitelist' },
       });
-      expect(createIdentityRequestResult.creation_block_height).to.be.a(
-        'string',
-      );
-      const splittedCreationBlockHeight = createIdentityRequestResult.creation_block_height.split(
-        ':',
-      );
-      expect(splittedCreationBlockHeight).to.have.lengthOf(2);
-      expect(splittedCreationBlockHeight[0]).to.have.lengthOf.at.least(1);
-      expect(splittedCreationBlockHeight[1]).to.have.lengthOf.at.least(1);
-    });
-
-    it('IdP (idp2) should receive message queue send success (To idp3) callback', async function () {
-      this.timeout(15000);
-      await receiveMessagequeueSendSuccessCallback({
-        nodeId: 'idp2',
-        requestId,
-        mqSendSuccessCallbackPromise: mqSendSuccessIdp2ToIdp3CallbackPromise,
-        destinationNodeId: 'idp3',
-      });
-    });
-
-    it('1st IdP should receive create identity request', async function () {
-      this.timeout(15000);
-      const incomingRequest = await incomingRequestPromise.promise;
-      expect(incomingRequest).to.deep.include({
-        mode: 3,
-        request_id: requestId,
-        reference_group_code: referenceGroupCode,
-        request_message: createIdentityRequestMessage,
-        request_message_hash: hash(
-          createIdentityRequestMessage + incomingRequest.request_message_salt,
-        ),
-        requester_node_id: 'idp2',
-        min_ial: 1.1,
-        min_aal: 1,
-        data_request_list: [],
-      });
-
-      expect(incomingRequest.creation_time).to.be.a('number');
-      expect(incomingRequest.creation_block_height).to.be.a('string');
-      const splittedCreationBlockHeight = incomingRequest.creation_block_height.split(
-        ':',
-      );
-      expect(splittedCreationBlockHeight).to.have.lengthOf(2);
-      expect(splittedCreationBlockHeight[0]).to.have.lengthOf.at.least(1);
-      expect(splittedCreationBlockHeight[1]).to.have.lengthOf.at.least(1);
-      expect(incomingRequest.request_timeout).to.be.a('number');
-
-      // requestMessageHash = incomingRequest.request_message_hash;
-    });
-
-    it('IdP should get request_message_padded_hash successfully', async function () {
-      identityForResponse = db.idp3Identities.find(
-        (identity) =>
-          identity.namespace === namespace &&
-          identity.identifier === identifier,
-      );
-
-      responseAccessorId = identityForResponse.accessors[0].accessorId;
-      let accessorPublicKey =
-        identityForResponse.accessors[0].accessorPublicKey;
-
-      const testResult = await getAndVerifyRequestMessagePaddedHashTest({
-        callApiAtNodeId: 'idp3',
-        idpNodeId: 'idp3',
-        requestId,
-        incomingRequestPromise,
-        accessorPublicKey,
-        accessorId: responseAccessorId,
-      });
-      requestMessagePaddedHash = testResult.verifyRequestMessagePaddedHash;
-    });
-
-    it('1st IdP should create response (accept) successfully', async function () {
-      this.timeout(10000);
-
-      let accessorPrivateKey =
-        identityForResponse.accessors[0].accessorPrivateKey;
-
-      const signature = createResponseSignature(
-        accessorPrivateKey,
-        requestMessagePaddedHash,
-      );
-
-      const response = await idpApi.createResponse('idp3', {
-        reference_id: idpReferenceId,
-        callback_url: config.IDP3_CALLBACK_URL,
-        request_id: requestId,
-        ial: 2.3,
-        aal: 3,
-        status: 'accept',
-        accessor_id: responseAccessorId,
-        signature,
-      });
-      expect(response.status).to.equal(202);
-    });
-
-    it('IdP shoud receive callback create response result with success = true', async function () {
-      const responseResult = await responseResultPromise.promise;
-      expect(responseResult).to.deep.include({
-        node_id: 'idp3',
-        type: 'response_result',
-        reference_id: idpReferenceId,
-        request_id: requestId,
-        success: true,
-      });
-    });
-
-    it('IdP (idp3) should receive message queue send success (To idp2) callback', async function () {
-      this.timeout(15000);
-      await receiveMessagequeueSendSuccessCallback({
-        nodeId: 'idp3',
-        requestId,
-        mqSendSuccessCallbackPromise: mqSendSuccessIdp3ToIdp2CallbackPromise,
-        destinationNodeId: 'idp2',
-      });
-    });
-
-    it('Should verify IdP response signature successfully', async function () {
-      this.timeout(15000);
-      const identity = db.idp3Identities.find(
-        (identity) =>
-          identity.namespace === namespace &&
-          identity.identifier === identifier,
-      );
-
-      let accessorPrivateKey = identity.accessors[0].accessorPrivateKey;
-
-      await verifyResponseSignature({
-        callApiAtNodeId: 'idp3',
-        requestId,
-        requestMessagePaddedHash,
-        accessorPrivateKey,
-      });
-    });
-
-    it('Identity should be created successfully', async function () {
-      this.timeout(15000);
-      const createIdentityResult = await createIdentityResultPromise.promise;
-      expect(createIdentityResult).to.deep.include({
-        reference_id: referenceId,
-        request_id: requestId,
-        success: true,
-      });
-      expect(createIdentityResult.reference_group_code).to.equal(
-        referenceGroupCode,
-      );
-
-      //referenceGroupCode = createIdentityResult.reference_group_code;
-
-      const response = await commonApi.getRelevantIdpNodesBySid('idp2', {
-        namespace,
-        identifier,
-      });
-      const idpNodes = await response.json();
-      const idpNode = idpNodes.find((idpNode) => idpNode.node_id === 'idp2');
-      expect(idpNode).to.not.be.undefined;
-      expect(idpNodes).to.be.an('array').that.to.have.lengthOf(2);
-      expect(idpNode.mode_list).to.be.an('array').that.include(2);
-
-      db.idp2Identities.push({
-        referenceGroupCode,
-        mode: 3,
-        namespace,
-        identifier,
-        accessors: [
-          {
-            accessorId,
-            accessorPrivateKey,
-            accessorPublicKey,
-          },
-        ],
-      });
-    });
-
-    it('After create identity IdP (idp3) that associated with this sid should receive identity notification callback', async function () {
-      this.timeout(15000);
-      const notificationCreateIdentity = await notificationCreateIdentityPromise.promise;
-      //const IdP2notificationCreateIdentity = await notificationCreateIdentityPromise.promise;
-      expect(notificationCreateIdentity).to.deep.include({
-        node_id: 'idp2',
-        type: 'identity_modification_notification',
-        reference_group_code: referenceGroupCode,
-        action: 'create_identity',
-        actor_node_id: 'idp2',
-      });
-    });
-
-    it('Special request status for create identity (mode 3) should be completed and closed', async function () {
-      this.timeout(10000);
-      //wait for API close request
-      await wait(3000);
-      const response = await commonApi.getRequest('idp2', { requestId });
-      const responseBody = await response.json();
-      expect(responseBody).to.deep.include({
-        request_id: requestId,
-        min_idp: 1,
-        min_aal: 1,
-        min_ial: 1.1,
-        request_timeout: 86400,
-        idp_id_list: ['idp3'],
-        data_request_list: [],
-        closed: true,
-        timed_out: false,
-        mode: 3,
-        status: 'completed',
-        requester_node_id: 'idp2',
-      });
-      expect(responseBody.creation_block_height).to.be.a('string');
-      const splittedCreationBlockHeight = responseBody.creation_block_height.split(
-        ':',
-      );
-      expect(splittedCreationBlockHeight).to.have.lengthOf(2);
-      expect(splittedCreationBlockHeight[0]).to.have.lengthOf.at.least(1);
-      expect(splittedCreationBlockHeight[1]).to.have.lengthOf.at.least(1);
-    });
-
-    it('After create identity this sid should be existing on platform ', async function () {
-      const response = await identityApi.getIdentityInfo('idp2', {
-        namespace,
-        identifier,
-      });
-      expect(response.status).to.equal(200);
-      const responseBody = await response.json();
-      expect(responseBody.reference_group_code).to.equal(referenceGroupCode);
-    });
-
-    it('After create identity should get identity ial successfully', async function () {
-      const response = await identityApi.getIdentityIal('idp2', {
-        namespace,
-        identifier,
-      });
-      expect(response.status).to.equal(200);
-      const responseBody = await response.json();
-      expect(responseBody.ial).to.equal(2.3);
-    });
-
-    it('Should get relevant IdP nodes by sid successfully', async function () {
-      this.timeout(15000);
-
-      const response = await commonApi.getRelevantIdpNodesBySid('idp3', {
-        namespace,
-        identifier,
-      });
-
-      const responseBody = await response.json();
-      expect(responseBody).to.be.an('array').that.to.have.lengthOf(2);
-      let idp = responseBody.find((node) => node.node_id === 'idp3');
-      expect(idp.ial).to.equal(2.3);
-      idp = responseBody.find((node) => node.node_id === 'idp2');
-      expect(idp.ial).to.equal(2.3);
     });
 
     after(function () {
