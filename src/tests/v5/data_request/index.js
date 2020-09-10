f5e33142bccb77f149cb329f7399aed9610c112a
Jirapus <jirapus@blockfint.com>
2020-09-10 15:16:47 +0700

    Add more test AS data response through callback

--- src/tests/v5/data_request/index.js
+++ src/tests/v5/data_request/index.js
@@ -11,6 +11,7 @@ describe('Data request flow', function() {
   require('./1_idp_1_as_2_services_mode_3');
   require('./1_idp_2_as_1_service_mode_3');
   require('./1_idp_2_as_2_service_mode_3');
+  require('./as_data_response_through_callback');
   require('./large_data_size');
   require('./large_data_size_response_through_callback');
   require('./too_large_data_size_response_through_callback');
