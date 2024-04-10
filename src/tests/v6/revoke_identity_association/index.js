6f5e07aeaeaad1b20752cd461728acccc21640dc
Raphassit Leelachaikul <raphassitlee@gmail.com>
2024-04-10 18:20:34 +0700

    formatting

--- src/tests/v6/revoke_identity_association/index.js
+++ src/tests/v6/revoke_identity_association/index.js
@@ -1,9 +1,9 @@
-describe('Revoke identity association', function() {
+describe('Revoke identity association', function () {
   require('./revoke_identity_association_mode_2');
   require('./revoke_identity_association_mode_3');
   require('./revoke_identity_association_mode_3_only_1_idp');
 
-  require('./recreate_after_revoke_identity_association_mode_2')
+  require('./recreate_after_revoke_identity_association_mode_2');
 
   require('./error_response');
 });
