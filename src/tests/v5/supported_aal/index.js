28fa87733cec828f6c06c011840bd6ef33b943e8
Jirapus Eiamsomboon <jirapus@blockfint.com>
2024-07-03 15:17:55 +0700

    Add more test case full flow mode 1 and mode 2 to supported ial,aal api v5

--- src/tests/v5/supported_aal/index.js
+++ src/tests/v5/supported_aal/index.js
@@ -1,4 +1,8 @@
 describe('Supported AAL tests', function () {
   require('./create_request');
   require('./create_request_with_invalid_aal');
+
+  //full flow
+  require('./create_request_mode_1');
+  require('./create_request_mode_2');
 });
